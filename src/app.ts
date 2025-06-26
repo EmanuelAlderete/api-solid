d87d9b26cbf477e70b9d480e526b07639b693264
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-26 13:53:06 -0300

    feat: remove unused imports from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import fastify from 'fastify'
-import { z } from 'zod'
 import { register } from './http/controllers/register.controller'
-import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
