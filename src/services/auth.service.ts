e59dbee48edefaf004a9059d49e998084bb049fb
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-27 10:13:39 -0300

    feat: implement AuthService for user authentication and add InvalidCredentialsError

---
+++ src/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { compare } from 'bcryptjs'
+import type { User } from 'generated/prisma'
+import type { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+type AuthServiceRequest = {
+	email: string
+	password: string
+}
+
+type AuthServiceResponse = {
+	user: User
+}
+
+export class AuthService {
+	constructor(private usersRepository: UsersRepository) {}
+	async execute({
+		email,
+		password,
+	}: AuthServiceRequest): Promise<AuthServiceResponse> {
+		const user = await this.usersRepository.findByEmail(email)
+
+		if (!user) {
+			throw new InvalidCredentialsError()
+		}
+
+		const doesPasswordMatches = await compare(password, user.password_hash)
+
+		if (!doesPasswordMatches) {
+			throw new InvalidCredentialsError()
+		}
+
+		return {
+			user,
+		}
+	}
+}
