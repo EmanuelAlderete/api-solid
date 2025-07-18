b5800b083bb44529bf9b8fe9b65407f7a3c0b136
Emanuel Alderete <emanuel.alderete@jusfy.com.br>
2025-07-18 14:38:38 -0300

    fix: update authentication and user profile tests to use await with expect for proper async handling

--- src/services/auth/auth.spec.ts
+++ src/services/auth/auth.spec.ts
@@ -29,7 +29,7 @@ describe('Authentication Service', () => {
 	})
 
 	it('should not be able to authenticate with wrong email', async () => {
-		expect(() =>
+		await expect(() =>
 			sut.execute({
 				email: 'johntestser@gmail.com',
 				password: '123456',
@@ -44,7 +44,7 @@ describe('Authentication Service', () => {
 			password_hash: await hash('123456', 6),
 		})
 
-		expect(() =>
+		await expect(() =>
 			sut.execute({
 				email: 'johntester@gmail.com',
 				password: 'wrongpassword',
